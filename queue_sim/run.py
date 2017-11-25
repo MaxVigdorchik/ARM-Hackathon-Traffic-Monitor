cdf66945ca5bf9d159e447e04f43901e7b38afe8
Soham-G <sohamgarg@hotmail.co.uk>
2017-11-25 22:08:57 +0000

    Bug fix

--- queue_sim/run.py
+++ queue_sim/run.py
@@ -17,23 +17,30 @@ class Node:
         return self.lat
 
 class Device:
-    def __init__(self, DevID):
+    def __init__(self, DevID, NodeAID, NodeBID, Inflow):
         self.DevID = DevID
-        self.NodeAID = "Not Set"
-        self.NodeBID = "Not Set"
-        self.Inflow = True
-    def NodeIn(self):
-        return self.NodeIn
-    def NodeOut(self):
-        return self.NodeOut
+        self.NodeAID = NodeAID
+        self.NodeBID = NodeBID
+        self.Inflow = Inflow
+    def DevID(self):
+        return self.DevID
+    def NodeAID(self):
+        return self.NodeAID
+    def NodeBID(self):
+        return self.NodeBID
     def Inflow(self):
         return self.Inflow
 
 NodeA = Node(1, 0, 10)
-DeviceA = Device(1)
-# nodes = {'NodeID': NodeA.NodeID, 'Long': NodeA.long, 'Lat': NodeA.lat}
-nodes = {NodeA.NodeID: NodeA.long, NodeA.lat}
-devices = {}
+NodeB = Node(2, 0, 20)
+DeviceA = Device(1, 1, 2, True)
+DeviceB = Device(2, 1, 2, False)
+
+nodes = {NodeA.NodeID(): [NodeA.long(), NodeA.lat()]}
+devices = {DeviceA.DevID(): [DeviceA.NodeAID(), DeviceA.NodeBID(), DeviceA.Inflow()]}
+
+print(nodes)
+print(devices)
 
 
 
